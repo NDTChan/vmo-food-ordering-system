80f69847a26d67aea06d73ea7c2426dc045b91fc
TeaHan <thanhnd@vmogroup.com>
2022-12-28 15:45:06 +0700

    Messaging module Adding Publisher implementations

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -2,8 +2,8 @@ package com.food.ordering.system.order.service.messaging.mapper;
 
 import com.food.ordering.system.entity.Order;
 import com.food.ordering.system.event.OrderCreatedEvent;
-import com.food.ordering.system.kafka.order.avro.model.PaymentOrderStatus;
-import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.event.OrderPaidEvent;
+import com.food.ordering.system.kafka.order.avro.model.*;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -21,8 +21,6 @@ public class OrderMessagingDataMapper {
                 .setCreatedAt(orderCreatedEvent.getCreatedAt().toInstant())
                 .setPaymentOrderStatus(PaymentOrderStatus.PENDING)
                 .build();
-
-
     }
 
     public PaymentRequestAvroModel orderCancelledEventToPaymentRequestAvroModel(OrderCreatedEvent orderCancelledEvent) {
@@ -36,7 +34,25 @@ public class OrderMessagingDataMapper {
                 .setCreatedAt(orderCancelledEvent.getCreatedAt().toInstant())
                 .setPaymentOrderStatus(PaymentOrderStatus.PENDING)
                 .build();
+    }
 
-
+    public RestaurantApprovalRequestAvroModel
+    orderPaidEventToRestaurantApprovalRequestAvroModel(OrderPaidEvent orderPaidEvent) {
+        Order order = orderPaidEvent.getOrder();
+        return RestaurantApprovalRequestAvroModel.newBuilder()
+                .setId(UUID.randomUUID().toString())
+                .setSagaId("")
+                .setOrderId(order.getId().getValue().toString())
+                .setRestaurantId(order.getRestaurantId().getValue().toString())
+//                .setRestaurantOrderStatus(RestaurantOrderStatus
+//                        .valueOf(order.getStatus().name()))
+                .setProducts(order.getItems().stream().map(orderItem ->
+                        Product.newBuilder()
+                                .setId(orderItem.getProduct().getId().getValue().toString())
+                                .setQuantity(orderItem.getQuantity())
+                                .build()).toList())
+                .setPrice(order.getPrice().amount())
+                .setCreatedAt(orderPaidEvent.getCreatedAt().toInstant())
+                .build();
     }
 }
---
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/OrderKafkaMessageHelper.java
@@ -0,0 +1,34 @@
+package com.food.ordering.system.order.service.messaging.publisher.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Slf4j
+@Component
+public class OrderKafkaMessageHelper {
+    public <T> ListenableFutureCallback<SendResult<String, T>> getKafkaCallback(String paymentResponseTopicName, T paymentRequestAvroModel, String orderId, String requestAvroModelName) {
+        return new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Error while sending " + requestAvroModelName +
+                        " message {} to topic {}", paymentRequestAvroModel.toString(), paymentResponseTopicName, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, T> result) {
+                RecordMetadata metadata = result.getRecordMetadata();
+                log.info("Received successful response from Kafka for order id: {}" +
+                                " Topic: {} Partition: {} Offset: {} Timestamp: {}",
+                        orderId,
+                        metadata.topic(),
+                        metadata.partition(),
+                        metadata.offset(),
+                        metadata.timestamp()
+                );
+            }
+        };
+    }
+}
