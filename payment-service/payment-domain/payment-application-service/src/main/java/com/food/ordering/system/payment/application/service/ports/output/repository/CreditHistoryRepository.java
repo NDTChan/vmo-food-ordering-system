f2697718e392a3bb52d4ac221044137030eb0ea7
TeaHan <justducthanh105mta@gmail.com>
2022-12-30 15:02:31 +0700

    Application Service domain module Adding Mapper, Config and Ports

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/application/service/ports/output/repository/CreditHistoryRepository.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.payment.application.service.ports.output.repository;
+
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.payment.service.domain.entity.CreditHistory;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CreditHistoryRepository {
+    CreditHistory save(CreditHistory creditHistory);
+    Optional<List<CreditHistory>> findByCustomerId(CustomerId orderId);
+}
