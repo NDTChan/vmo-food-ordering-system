78c7d9527adf2a0b993787ce6849a0ec922d4b1f
TeaHan <thanhnd@vmogroup.com>
2023-01-04 15:51:50 +0700

    Implementing  Container module

---
+++ restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/container/RestaurantServiceApplication.java
@@ -0,0 +1,17 @@
+package com.food.ordering.system.restaurant.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = {"com.food.ordering.system.data.access.restaurant",
+        "com.food.ordering.system.common.data.access"})
+@EntityScan(basePackages = {"com.food.ordering.system.data.access.restaurant",
+        "com.food.order.system.common.data.access"})
+@SpringBootApplication(scanBasePackages = "com.food.ordering")
+public class RestaurantServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
+}
